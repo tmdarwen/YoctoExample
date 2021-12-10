1d259765a77305ea613e09c8244aeb556675d267
Terence Darwen <terence.darwen@stereotaxis.com>
2021-12-10 17:29:26 -0600

    Modifications for thrid party dependency

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <future>
+#include "signalrclient/hub_connection.h"
+#include "signalrclient/hub_connection_builder.h"
+#include "signalrclient/signalr_value.h"
 
 int main(int argc, char *argv[])
 {
-   std::cout << "Hello World!" << std::endl;
-   return 0;
+    std::promise<void> start_task;
+    signalr::hub_connection connection = signalr::hub_connection_builder::create("http://localhost:5000/hub").build();
+    
+    connection.on("Echo", [](const std::vector<signalr::value>& m)
+    {
+        std::cout << m[0].as_string() << std::endl;
+    });
+    
+    connection.start([&start_task](std::exception_ptr exception) {
+        start_task.set_value();
+    });
+    
+    start_task.get_future().get();
+    
+    std::promise<void> send_task;
+    std::vector<signalr::value> args { "Hello world" };
+    connection.invoke("Echo", args, [&send_task](const signalr::value& value, std::exception_ptr exception) {
+        send_task.set_value();
+    });
+    
+    send_task.get_future().get();
+    
+    std::promise<void> stop_task;
+    connection.stop([&stop_task](std::exception_ptr exception) {
+        stop_task.set_value();
+    });
+    
+    stop_task.get_future().get();
+
+    return 0;
 }
